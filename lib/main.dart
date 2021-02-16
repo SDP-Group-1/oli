e90b01f21863487da17ab313d41dc85deaae1d45
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-17 00:22:17 +0400

    new fonts
    added custom font to app - Yaldevi Colombo

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OLI',
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
+      theme:
+          ThemeData(primaryColor: Colors.blue, fontFamily: 'Yaldevi Colombo'),
       home: HomePage(),
     );
   }
@@ -20,7 +19,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('OLI'),
+        title: Text('OLI', style: TextStyle(fontWeight: FontWeight.w700)),
       ),
       body: HomeButtons(),
     );
@@ -32,24 +31,25 @@ class HomeButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Call OLI'),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Setup'),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Fall Sensor Demo'),
-            ),
-          ],
-        )
-    );
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          child:
+              Text('Call OLI', style: TextStyle(fontWeight: FontWeight.w900)),
+        ),
+        const SizedBox(height: 12),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Setup', style: TextStyle(fontWeight: FontWeight.w900)),
+        ),
+        const SizedBox(height: 12),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Fall Sensor Demo',
+              style: TextStyle(fontWeight: FontWeight.w900)),
+        ),
+      ],
+    ));
   }
 }
