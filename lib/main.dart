b44806196008518450b12d8d7c48bb7ec58f7d0a
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-17 23:04:31 +0400

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OLI',
-      theme:
-          ThemeData(primaryColor: Colors.blue, fontFamily: 'Yaldevi Colombo'),
+      theme: ThemeData(
+          primaryColor: Color(0xffFFEFEB),
+          fontFamily: 'Yaldevi Colombo',
+          scaffoldBackgroundColor: Color(0xffFFEFEB),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(primary: Color(0xff2B3964)))),
       home: HomePage(),
     );
   }
@@ -18,38 +22,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('OLI', style: TextStyle(fontWeight: FontWeight.w700)),
-      ),
-      body: HomeButtons(),
-    );
+        body: Padding(
+            padding: EdgeInsets.only(left: 0, top: 100.0, right: 0, bottom: 0),
+            child: new Column(
+              children: [
+                new Text('O.L.I.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff2B3964),
+                        fontSize: 100)),
+                new Text('operational lifting intern',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff2B3964),
+                        fontSize: 30)),
+                HomeButtons()
+              ],
+            )
+            //   appBar: AppBar(
+            //       title: Text('OLI',
+            //           style: TextStyle(
+            //               fontWeight: FontWeight.w700, color: Color(0xff2B3964))),
+            //       backgroundColor: Color.fromRGBO(255, 239, 235, 1),
+            //       centerTitle: true,
+            //       shadowColor: Color.fromRGBO(0, 0, 0, 0),
+            //       toolbarHeight: 100),
+            //   body: HomeButtons(),
+            ));
   }
 }
 
 class HomeButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ElevatedButton(
-          onPressed: () {},
-          child:
-              Text('Call OLI', style: TextStyle(fontWeight: FontWeight.w900)),
-        ),
-        const SizedBox(height: 12),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text('Setup', style: TextStyle(fontWeight: FontWeight.w900)),
-        ),
-        const SizedBox(height: 12),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text('Fall Sensor Demo',
-              style: TextStyle(fontWeight: FontWeight.w900)),
-        ),
-      ],
-    ));
+    return Padding(
+        padding: EdgeInsets.only(left: 0, top: 40.0, right: 0, bottom: 0),
+        child: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Call OLI',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Color(0xffDB5461))),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Setup',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Color(0xffDB5461))),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Fall Sensor Demo',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Color(0xffDB5461))),
+            ),
+          ],
+        )));
   }
 }
