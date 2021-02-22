5a80a8e1c51a5bfea8bb1c928d50ec2942c01d29
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-22 12:41:37 +0400

    #4 added templates + dependencies
    - added named routes template
    - background page that should show up set up
    - responsive screen dependency added

---
+++ lib/background.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class BackgroundSensors extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sensor screen")),
+    );
+  }
+}
