e302348ecd27164b4b3fe70aa7febafa967a3c01
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-23 23:10:00 +0400

    set up stateful widget

--- lib/background.dart
+++ lib/background.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
+import 'dart:async';
+import 'dart:isolate';
 
 class BackgroundSensors extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Sensor screen")),
     );
+  }
+}
+
+class BackgroundActivity extends StatefulWidget {
+  @override
+  _BackgroundActivityState createState() => _BackgroundActivityState();
+}
+
+class _BackgroundActivityState extends State<BackgroundActivity> {
+  Isolate _isolate;
+  bool _running = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Sensing movement'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('Something here'),
+            ],
+          ),
+        ));
   }
 }
