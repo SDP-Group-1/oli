7b6253322427f329509c6451d7e64f465edc03c6
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-26 19:37:35 +0400

    tried something

--- lib/background.dart
+++ lib/background.dart
@@ -6,8 +6,8 @@ import 'dart:isolate';
 class BackgroundSensors extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sensor screen")),
-    );
+        appBar: AppBar(title: Text("Sensor screen")),
+        body: BackgroundActivity());
   }
 }
 
@@ -17,22 +17,59 @@ class BackgroundActivity extends StatefulWidget {
 }
 
 class _BackgroundActivityState extends State<BackgroundActivity> {
-  Isolate _isolate;
-  bool _running = false;
-
+  //each list element can be linked to one sensor? instead of using
+  //broadcast stream
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+  List<double> _accelerometerValues, _gyroscopeValues, _userAccelerometerValues;
   @override
   Widget build(BuildContext context) {
+    final List<String> accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Sensing movement'),
-        ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              new Text('Something here'),
-            ],
-          ),
-        ));
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Accelerometer: $accelerometer',
+                    style: TextStyle(fontSize: 32)),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+          )
+        ]));
   }
-}
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        _accelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions.add(gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions
+        .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+  }
+} //_BACKGROUND ACTIVITY STATE
