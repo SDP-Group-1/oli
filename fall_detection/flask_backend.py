2e9ec1256b131d80ce3d65b2c56ee176ddb35673
MadnoScientist <58093835+MadnoScientist@users.noreply.github.com>
2021-03-25 00:49:55 +0800

    The fall_detection folder now contains two file:  - clf_pickle is the exported classifier model.  - flask_backend.py is the script that is going to process the csv file and do classification, and it is an API for flutter to call by http request.

---
+++ fall_detection/flask_backend.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify
+import pandas as pd
+import numpy as np
+import pickle
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+
+def load_model():
+    f = open('clf_pickle','rb')
+    s = f.read()
+    classifier=pickle.loads(s)
+    f.close()
+    return classifier
+
+def parse(fileName:str)->np.array:
+    sample = pd.read_csv(fileName)
+    df = pd.DataFrame(columns=['mean_smv', 'std_smv', 'std_mless',
+                               'max_smv', 'min_smv', 'slope', 'duration'])
+    # total_frames = sample.shape[0]
+    smv_list = np.array([])
+    for line in range(0, sample.shape[0]):
+        acc_x = sample['a_x']
+        acc_y = sample['a_y']
+        acc_z = sample['a_z']
+        smv_list = np.append(smv_list,
+                             np.sqrt(pow(acc_x,2) + pow(acc_y,2) + pow(acc_z,2)))
+
+    min_idx = np.argmin(smv_list)
+    max_idx = np.argmax(smv_list)
+
+    mean_smv = np.mean(smv_list)
+    std_smv = np.std(smv_list)
+    std_motionless_smv = np.std(smv_list[min_idx:])
+    max_smv = np.max(smv_list)
+    min_smv = np.min(smv_list)
+    # duration need to confirm by the sampling rate
+    # duration = min_idx - max_idx
+    # calculate slope fature
+    max_line = sample.iloc[max_idx]
+    min_line = sample.iloc[min_idx]
+    max_smv_x = max_line['acc_x']
+    max_smv_y = max_line['acc_y']
+    max_smv_z = max_line['acc_z']
+    min_smv_x = min_line['acc_x']
+    min_smv_y = min_line['acc_y']
+    min_smv_z = min_line['acc_z']
+    slope = np.sqrt(pow(max_smv_x - min_smv_x, 2)
+                   + pow(max_smv_y - min_smv_y, 2)
+                   + pow(max_smv_z - min_smv_z, 2))
+
+    parsed_sample = np.array([mean_smv, std_smv, std_motionless_smv, max_smv, min_smv, slope, duration])
+
+    return parsed_sample
+
+def predict(clf, sample_window)->bool:
+    result = clf.predict(sample_window)
+    if result == 1:
+        return True
+    else:
+        return False
+if __name__ == '__main__':
+    app.run()
