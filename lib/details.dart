543ab2b46e3f7eb9f65965a5fd3b57601b635811
Ap0806 <57600509+parna86@users.noreply.github.com>
2021-03-25 18:11:52 +0530

    brought back the post fall screen

--- lib/details.dart
+++ lib/details.dart
@@ -25,32 +25,27 @@ class _DetailFormState extends State<DetailForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'Enter your name',
-              labelText: 'Name',
-            ),
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.person),
+            hintText: 'Enter your name',
+            labelText: 'Name',
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.phone),
-              hintText: 'Input emergency contact number',
-              labelText: 'Emergency contact',
-            ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.phone),
+            hintText: 'Input emergency contact number',
+            labelText: 'Emergency contact',
           ),
-          Center(
+        ),
+        Center(
             child: Container(
-              child: RaisedButton(
-                child: Text('Submit'),
-                  onPressed: null)
-            )
-          )
-        ],
-      )
-    );
+                child: RaisedButton(child: Text('Submit'), onPressed: null)))
+      ],
+    ));
   }
 }
