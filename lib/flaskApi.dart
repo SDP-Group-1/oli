0c3ec9a216f156614cfa4f746b999180be6fd874
MadnoScientist <58093835+MadnoScientist@users.noreply.github.com>
2021-03-25 18:28:08 +0800

    Flutter side api to access flask.

---
+++ lib/flaskApi.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<dynamic> getPredict(csv_path) async {
+  final response = await http.post(
+      'https//127.0.0.1:5000/',
+      headers: <String, String> {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: {'path' :csv_path});
+
+  if (response.statusCode == 200) {
+    final decoded = json.decode(response.body) as Map<String, dynamic>;
+    return decoded['result'];
+  }
+}
