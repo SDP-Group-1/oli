1ddb275e28833e52f3602c3caeaa71666b7e7c3c
Pingqi Li <58093835+MadnoScientist@users.noreply.github.com>
2021-03-27 00:47:46 +0800

    Update flaskApi.dart

--- lib/flaskApi.dart
+++ lib/flaskApi.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as http;
 
 Future<int> getPredict(csv_path) async {
   final response = await http.post(
-    'https//127.0.0.1:5000/',
+    // change the ip to your computer's
+    'http://192.168.50.144:5000/',
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
@@ -11,6 +12,7 @@ Future<int> getPredict(csv_path) async {
       'path': csv_path,
     }),
   );
+  print(csv_path);
   print(response.statusCode);
   if (response.statusCode == 200) {
     final decoded = json.decode(response.body) as Map<String, int>;
