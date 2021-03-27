9e98d946183338d60f123f313e370d7826903a9f
Pingqi Li <58093835+MadnoScientist@users.noreply.github.com>
2021-03-28 03:06:31 +0800

    Update flask_backend.py
    Change to fit svm classifier and new feature set

--- fall_detection/flask_backend.py
+++ fall_detection/flask_backend.py
@@ -8,21 +8,27 @@ import pickle
 import json
 
 def load_model():
-    model_path = os.path.join(relative_path, 'clf_pickle')
+    model_path = os.path.join(relative_path, 'svm')
+    scaler_path = os.path.join(relative_path, 'scaler')
     f = open(model_path,'rb')
     s = f.read()
-    classifier=pickle.loads(s)
+    svm = pickle.loads(s)
     f.close()
-    return classifier
+    g = open(scaler_path,'rb')
+    s = g.read()
+    scaler = pickle.loads(s)
+    g.close()
+    return svm, scaler
 
 def parse(csv_str:str):
     f = open('{}/window.csv'.format(relative_path), 'w')
     f.write(csv_str)
     f.close()
     sample = pd.read_csv('{}/window.csv'.format(relative_path))
-    df = pd.DataFrame(columns=['mean_smv', 'std_smv', 'std_mless',
+    df = pd.DataFrame(columns=['mean_ax','mean_ay','mean_az',
+                                'mean_smv', 'std_smv', 'std_mless',
                                'max_smv', 'min_smv', 'slope', 'duration'])
-    # total_frames = sample.shape[0]
+    
     smv_list = []
     for line in range(0, sample.shape[0]):
         acc_x = sample['a_x'][line]
@@ -32,6 +38,9 @@ def parse(csv_str:str):
     smv_list = np.asarray(smv_list)
     min_idx = np.argmin(smv_list)
     max_idx = np.argmax(smv_list)
+    mean_ax = np.mean(sample['a_x'])
+    mean_ay = np.mean(sample['a_y'])
+    mean_az = np.mean(sample['a_z'])
     mean_smv = np.mean(smv_list)
     std_smv = np.std(smv_list)
     std_motionless_smv = np.std(smv_list[min_idx:])
@@ -50,7 +59,9 @@ def parse(csv_str:str):
                    + pow(max_smv_y - min_smv_y, 2)
                    + pow(max_smv_z - min_smv_z, 2))
 
-    parsed_sample = np.array([mean_smv, std_smv, std_motionless_smv, max_smv, min_smv, slope, duration])
+    parsed_sample = np.array([mean_ax, mean_ay, mean_az, mean_smv,
+                            std_smv, std_motionless_smv, max_smv, 
+                            min_smv, slope, duration])
 
     return parsed_sample.reshape(1,-1)
 
@@ -60,10 +71,11 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def predict():
     csv_str = request.json['csv']
-    clf = load_model()
+    svm, scaler = load_model()
     sample = parse(csv_str)
     print(sample)
-    result = clf.predict(sample)[0]
+    sample_nm = scaler.transform(sample)
+    result = svm.predict(sample_nm)[0]
     if result == 1:
         print('This is a fall event')
         return jsonify({'result': 'Fall'})
